501eac5ba9313357575df126832922d779d4d635
Hannah Swift <h.graceswift@gmail.com>
2016-07-12 21:19:30 -0500

    I am confused

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 export default class App {
   constructor(appElement) {
     this.appElement = appElement;
+    this.appElement = document.createElement('div');
+    this.appElement.classList.add('hands-list__item');
+    this.appElement.innerHTML = `
+    <figure class="card">
+      <div class="img-container">
+        <img src="http://www.placekitten.com/200/300" alt="" class="img-container__img">
+      </div>
+      <h3 class="number">8</h3>
+    </figure>`;
+  }
+
+  start() {
+    fetch(`https://card-proxy.herokuapp.com/decks/new`)
+      .then((res) => res.json())
+      .then((data) => {
+        data.results.forEach((card) => {
+          // Make a new playingCard
+
+          // Put the playingCard where the user can see it...
+          this.appElement.querySelector('.hands-list').appendChild(card.appElement);
+
+          // Tell the playingCard to fill in...
+        });
+      });
   }
 }
