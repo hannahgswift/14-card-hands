9ba0f9f5f4a7f2b2dcfe421eb41843012f9db846
Hannah Swift <h.graceswift@gmail.com>
2016-07-13 16:48:22 -0500

    Thank you wavy hands

--- src/app.js
+++ src/app.js
@@ -3,23 +3,23 @@ import Hand from 'hand';
 export default class App {
   constructor(appElement) {
     this.appElement = appElement;
-
     console.log('hihihi');
-
   }
 
   start() {
-    fetch(`https://card-proxy.herokuapp.com/decks/new`)
+    return fetch(`https://card-proxy.herokuapp.com/decks/new`)
       .then((res) => res.json())
       .then((data) => {
-
-        for (var i = 0; i < 5; i++) {
-          const newHand = new Hand(data.deck_id, this.appElement);
+        for (let i = 0; i < 5; i++) {
+          const newHand = new Hand(data.deck_id);
+          this.appElement.querySelector('.app-container')
+            .appendChild(newHand.element);
 
           newHand.getData().then(() => {
             newHand.render();
           });
         }
+        console.log('YELLOW');
       });
   }
 
--- src/hand.js
+++ src/hand.js
@@ -2,16 +2,8 @@ export default class Hand {
   constructor(deck_id) {
     this.deck_id = deck_id;
 
-    this.element = document.createElement('div');
-    this.element.classList.add('hands-list-item');
-    this.element.innerHTML = `
-        <figure class="card">
-          <div class="img-container">
-            <img src="http://www.placekitten.com/200/300" alt="" class="img-container__img">
-          </div>
-          <h3 class="number">8</h3>
-        </figure>`;
-
+    this.element = document.createElement('ul');
+    this.element.classList.add('hands-list');
   }
   getData() {
     return fetch(`https://card-proxy.herokuapp.com/decks/${this.deck_id}/draw?count=5`)
@@ -22,5 +14,33 @@ export default class Hand {
       });
   }
   render() {
+    const listItems = this.data.cards.map((card) => {
+      // make element look like:
+      // <li class="hands-list__item">
+      //   <figure class="card">
+      //     <div class="img-container">
+      //       <img src="http://www.placekitten.com/200/300" alt="" class="img-container__img">
+      //     </div>
+      //     <h3 class="number">8</h3>
+      //   </figure>
+      // </li>
+      const element = document.createElement('li');
+      element.innerText = card.value;
+      element.innerHTML = `
+      <li class="hands-list__item">
+        <figure class="card">
+          <div class="img-container">
+            <img src="${card.image}" alt="" class="img-container__img">
+          </div>
+          <h3 class="number"></h3>
+        </figure>
+      </li>`;
+
+      return element;
+    });
+
+    listItems.forEach((listItem) => {
+      this.element.appendChild(listItem)
+    });
   }
 }
